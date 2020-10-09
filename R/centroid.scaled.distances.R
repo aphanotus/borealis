e884f518ba8b4ffb708d8e19be0936a8fd9c2a0a
Dave Angelini <david.r.angelini@gmail.com>
2020-10-09 15:30:38 -0400

    Corrected algorithm

--- R/centroid.scaled.distances.R
+++ R/centroid.scaled.distances.R
@@ -1,6 +1,6 @@
 #' Calculate the matrix of distances between shape coordinates, scaled by centroid size
 #'
-#' @param m A matrix containing XY shape corrdinates for \emph{p} landmarks
+#' @param m A matrix containing XY shape coordinates for \emph{p} landmarks
 #' @param scale A logical argument for whether to scale the distances by centroid size
 #' @param symmetrical A logical argument for whether to keep the output matrix symmetrical.
 #'     If \code{symmetrical = FALSE} (the default) then the upper triangle and diagonal cells
@@ -15,14 +15,16 @@
 #' @examples
 #' x <- matrix(c(9,49, 24,54, 51,28, 81,54, 81,23, 86,32), ncol = 2, byrow = TRUE)
 #'
+#' centroid.scaled.distances(x)
+#' centroid.scaled.distances(x, symmetrical = TRUE)
+#' centroid.scaled.distances(x, symmetrical = TRUE, scale = FALSE)
+#'
+#' # cross validation
 #' df <- data.frame(matrix(nrow = dim(x)[1], ncol = dim(x)[1]))
 #' for (i in (1:dim(x)[1])) {
 #'   df[i,] <- unlist(apply(x, 1, distance, XY=x[i,]))
 #' }
 #' df
-#'
-#' centroid.scaled.distances(x, symmetrical = TRUE)
-#' centroid.scaled.distances(x, symmetrical = TRUE, scale = FALSE)
 #'
 
 centroid.scaled.distances <- function(m, scale = TRUE, symmetrical = FALSE)
@@ -34,8 +36,7 @@ centroid.scaled.distances <- function(m, scale = TRUE, symmetrical = FALSE)
   }
   if (!symmetrical) { df[upper.tri(df, diag = TRUE)] <- NA }
   if (scale) {
-    centroid <- apply(m,2,mean)
-    CS <- mean(apply(m,1, distance, XY=centroid))
+    CS <- sqrt(sum( (m[1,]-mean(m[1,]))^2 + (m[2,]-mean(m[2,]))^2 ) )
     df <- df / CS
   }
   return(df)
