92b6480b8227f5235405014da2a5308cdc71d01f
Dave Angelini <david.r.angelini@gmail.com>
2020-11-17 17:20:34 -0500

    Added ability to specify outliers in argument to align.procrustes

--- R/align.procrustes.R
+++ R/align.procrustes.R
@@ -1,44 +1,44 @@
-#' Generalized Prorustes alignment with interactive outlier detection and removal
+#' Generalized Procrustes alignment with interactive outlier detection and removal
 #'
 #' GPA is performed by a call to \code{\link[geomorph]{gpagen}}.
 #' If \code{outlier.analysis = TRUE} then the function will display the results of
 #' \code{\link[geomorph]{plotOutliers}}, then warp grids for the most extreme shapes.
-#' The user is prompted to select a cetain number of extreme shapes to exclude from
+#' The user is prompted to select a certain number of extreme shapes to exclude from
 #' a repeated Procrustes alignment. This processes is iterative, until the user indicates that
 #' it should stop or no shapes are requested for removal.
 #'
 #' @return Returns a list with Procrustes-aligned coordinates in \code{coords}, centroid size in \code{Csize}.
 #'     All other elements from \code{\link[geomorph]{gpagen}} are in the the element \code{gpagen}.
 #'     Any other list elements in the input are retained.
-#'     Data proveance is also retained in \code{provenance} and updated with details of the GPA..
+#'     Data provenance is also retained in \code{provenance} and updated with details of the GPA..
 #'
 #' @source   Dave Angelini \email{david.r.angelini@@gmail.com} [aut, cre]
 #'
 #' @references Adams and M.L. Collyer and A. Kaliontzopoulou. 2020. Geomorph: Software for geometric morphometric analyses. R package version 3.2.1. D.C. (\href{https://cran.r-project.org/package=geomorph}{Link})
 #'
-#' @param A A list or 3-dimensional array containing XY shape corrdinates for multiple specimens.
+#' @param A A list or 3-dimensional array containing XY shape coordinates for multiple specimens.
 #' @param curves An optional matrix defining which landmarks should be treated as semilandmarks by \code{gpagen}.
 #' @param show.plot.gpa A logical value specifying whether to plot the Procrustes-aligned landmarks.
-#' @param outlier.analysis A logical value specifying whether to poerform interactive outlier analysis.
-#' @param display.outliers The number of outlier shapes to display.
-#' @param provenance An object that should be retained for data provenance.
-#'     Unnecessary if \code{A} is a list that already contains a \code{provenance} element.
+#' @param outlier.analysis A logical value specifying whether to perform interactive outlier analysis.
+#'     Alternatively, a numeric vector can be provided indicating the index positions of specimens to remove.
+#'     A character vector of specimen names can also be provided, indicating the specimens to remove.
+#' @param display.outliers The number of outlier shapes to display in the interactive outlier analysis.
 #'
 #' @export
 #'
 #' @examples
 #' data("Bombus.forewings", package = "borealis")
 #'
-#' names(Bombus.forewings)
-#' cat(Bombus.forewings$provenance[[1]])
+#' fw.gpa <- align.procrustes(Bombus.forewings)
 #'
 #' fw.gpa <- align.procrustes(Bombus.forewings, show.plot.gpa = FALSE)
-#' plot(fw.gpa)
+#' plot(fw.gpa$gpagen)
 #'
 #' fw.gpa <- align.procrustes(Bombus.forewings, outlier.analysis = TRUE)
+#' fw.gpa <- align.procrustes(Bombus.forewings, outlier.analysis = 57)
+#' fw.gpa <- align.procrustes(Bombus.forewings, outlier.analysis = c("FJ190828-002","DRA190718-001"))
 #'
-#' names(fw.gpa$provenance)
-#' cat(fw.gpa$provenance$GPA)
+#' cat(fw.gpa$provenance$align.procrustes)
 #'
 
 align.procrustes <- function (
@@ -47,7 +47,6 @@ align.procrustes <- function (
   show.plot.gpa = TRUE,
   outlier.analysis = FALSE,
   display.outliers = 4,
-  provenance = NULL,
   ...
 )
 {
@@ -58,6 +57,7 @@ align.procrustes <- function (
 
   shape.data <- NULL
   output <- NULL
+  provenance <- NULL
 
   # Vet the input
   if (class(A)[1] %in% c("gpagen","list")) {
@@ -96,23 +96,58 @@ align.procrustes <- function (
   if(!is.null(curves)) {
     new.prov.details <- paste0(
       new.prov.details,
-      paste0("The follow matrix was used to define semilandmarks:\n\n|     |  LM |     |\n|:---:|:---:|:---:|\n| "),
+      paste0("The following matrix was used to define semilandmarks:\n\n|     |  LM |     |\n|:---:|:---:|:---:|\n| "),
       paste0(apply(curves,1,function(x) {paste0(sprintf("% 3d",x), collapse = " | ")}), collapse = " |\n| "),
       " |\n\n"
     )
   }
 
-  # GPA Plot
-  if (show.plot.gpa) {
-    plot(GPA)
-  }
-
   # Outlier analysis
   names.of.outliers.removed <- NULL
+  identified.outliers <- FALSE
+
+  # Allow outliers to be named in the argument as an index number
+  if (is.numeric(outlier.analysis) | is.character(outlier.analysis)) {
+    if (is.numeric(outlier.analysis)) {
+      if (length(outlier.analysis)>0) {
+        names.of.outliers.removed <- dimnames(shape.data)[[3]][outlier.analysis]
+        shape.data <- shape.data[,,-outlier.analysis]
+      }
+    } else {
+      # Allow outliers to be named in the argument as a specimen ID
+      if (is.character(outlier.analysis)) {
+        if (length(outlier.analysis)>0) {
+          names.of.outliers.removed <- outlier.analysis
+          x <- which(dimnames(shape.data)[[3]] %in% names.of.outliers.removed)
+          cat(x)
+          shape.data <- shape.data[,,-x]
+        }
+      }
+    }
+    new.prov.details <- paste0(new.prov.details,"The following ",length(names.of.outliers.removed)," specimens were removed as outliers: \n- ",paste0(names.of.outliers.removed, collapse = "\n- "),"\n\n")
+    GPA <- gpagen(shape.data, curves = curves, ...)
+    if (show.plot.gpa) {
+      plot(GPA)
+    }
+    outlier.analysis <- FALSE
+    identified.outliers <- TRUE
+  }
+
+  if (!outlier.analysis & !identified.outliers) {
+    if (show.plot.gpa) {
+      plot(GPA)
+    }
+  }
+
+  # Interactive outlier analysis
   if (outlier.analysis) {
 
-    # Pause if the user requests both the GPA plot and outlier analysis
-    if (show.plot.gpa) { invisible(readline(prompt="Press any key to continue.")) }
+    # Initial GPA Plot
+    if (show.plot.gpa) {
+      plot(GPA)
+      # Pause if the user requests both the GPA plot and outlier analysis
+      invisible(readline(prompt="Press any key to continue."))
+    }
 
     # Outlier Procrustes distances
     rounds.of.outlier.analysis <- 0
@@ -120,18 +155,19 @@ align.procrustes <- function (
     h <- ceiling(sqrt(display.outliers))
     w <- ifelse( ( h^2 - display.outliers) >= h, h-1, h )
 
-    readline(prompt="Press any key to continue.")
+    # readline(prompt="Press any key to continue.")
 
     continue.loop <- TRUE
+    ###################
     while (continue.loop) {
 
-      # Plot the worst outliers compared to the consensus GPA shape
+      # Plot the most extreme outliers to the consensus GPA shape
       par(mfrow=c(h,w))
       for (i in 1:display.outliers) {
         plotRefToTarget(GPA$consensus,GPA$coords[,,outliers[i]])
-        text(0,0.25,
-             paste('outlier #',i,': ',dimnames(shape.data)[[3]][outliers[i]],sep=''),
-             col='darkred', font=2)
+        title(main = paste0('outlier #',i,'  [',outliers[i],']\n',
+                           dimnames(shape.data)[[3]][outliers[i]]),
+              col.main = 'darkred', font.main = 1)
       }
       par(mfrow=c(1,1))
 
@@ -156,7 +192,7 @@ align.procrustes <- function (
         rounds.of.outlier.analysis <- rounds.of.outlier.analysis + 1
 
         # Repeat GPA
-        GPA <- gpagen(shape.data, ...)
+        GPA <- gpagen(shape.data, curves = curves, ...)
 
         if (show.plot.gpa) {
           plot(GPA)
@@ -176,6 +212,7 @@ align.procrustes <- function (
       } # End if/else  (rm.outliers > 0)
 
     } # End while-loop
+    ###################
 
     if (!is.null(names.of.outliers.removed)) {
       new.prov.details <- paste0(new.prov.details,"The following ",length(names.of.outliers.removed)," specimens were removed after ",rounds.of.outlier.analysis," rounds of outlier analysis: \n- ",paste0(names.of.outliers.removed, collapse = "\n- "),"\n\n")
